84143cfee9afdba47613862c2acb9ce0e631d16a
rubioz <jarubc@gmail.com>
2013-06-16 20:29:52 +0200

    Improved config options

--- linkedin/spiders/naive.py
+++ linkedin/spiders/naive.py
@@ -1,26 +1,19 @@
 from scrapy.spider import BaseSpider
-from scrapy.http import FormRequest, Request
+from scrapy.http import Request
 from scrapy.selector import HtmlXPathSelector
+from scrapy.conf import settings
 from scrapy import log
 from linkedin.items import LinkedinItem
 from urlparse import urljoin
-import itertools as it
-import re
-
-
-#def urljoin(a, b):
-    #return a + b
-
 
 class CompanyCrawler(BaseSpider):
     name = "linkedin"
     base_domain = 'http://www.linkedin.com'
-    #base_domain = 'http://webcache.googleusercontent.com/search?q=cache:http://www.linkedin.com'
     start_urls = [urljoin(base_domain, '/directory/companies/%s.html') %
-                  #s for s in 'abcdefghijklmnopqrstuvwxyz']
-                  s for s in 'x']
+                  s for s in settings['COMPANY_LETTERS']]
 
     def parse(self, response):
+
         hxs = HtmlXPathSelector(response)
 
         items = []
@@ -30,21 +23,10 @@ class CompanyCrawler(BaseSpider):
 
         for rel_path in relative_paths:
             full_path = urljoin(self.base_domain, rel_path)
-            request = Request(full_path, callback=self.parse_2nd_level)
-            items.append(request)
-        return items
-
-    def parse_2nd_level(self, response):
-        hxs = HtmlXPathSelector(response)
-
-        items = []
-        SECOND_LEVEL_DIR = '//*[@id="body"]/div/ul[2]/li/a/@href'
-
-        relative_paths = hxs.select(SECOND_LEVEL_DIR).extract()
-
-        for rel_path in relative_paths:
-            full_path = urljoin(self.base_domain, rel_path)
-            request = Request(full_path, callback=self.parse_company)
+            if 'depth' in response.meta and response.meta['depth'] == 1:
+                request = Request(full_path, callback=self.parse_company)
+            else:
+                request = Request(full_path, callback=self.parse)
             items.append(request)
         return items
 
@@ -61,10 +43,7 @@ class CompanyCrawler(BaseSpider):
         year = hxs.select(YEAR_XPATH).extract()
         website = hxs.select(SITE_XPATH).extract()
 
-        #if len(website) == 0:
-            #SITE_XPATH = '//*[@id="extra"]/div[2]/div[2]/div/dl/dd[3]/a/text()'
-            #website = hxs.select(SITE_XPATH).extract()
-
-        item = LinkedinItem(_id=response.url, country=country, name=company_name, foundation_year=year)
+        item = LinkedinItem(_id=response.url, country=country,
+                            name=company_name, foundation_year=year)
         item['website_link'] = website
         return item
---
+++ settings.py
@@ -0,0 +1,66 @@
+BOT_NAME = 'linkedin'
+LOG_LEVEL = 'DEBUG'
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.contrib.downloadermiddleware.redirect.RedirectMiddleware': 542,
+    'linkedin.middleware.ProxySelectorMiddleware': 543,
+    'linkedin.middleware.CustomUserAgentMiddleware': 545,
+}
+
+SPIDER_MIDDLEWARES = {
+    'scrapy.contrib.spidermiddleware.depth.DepthMiddleware': 546
+}
+
+ITEM_PIPELINES = ['linkedin.pipelines.CleanseTextPipeline',
+                  'linkedin.pipelines.MongoDBPipeline']
+SPIDER_MODULES = ['linkedin.spiders']
+NEWSPIDER_MODULE = 'linkedin.spiders'
+
+COOKIES_ENABLED = False
+REDIRECT_ENABLED = False
+
+RETRY_ENABLED = True
+RETRY_TIMES = 3
+RETRY_HTTP_CODES = [113, 302, 500, 502, 503, 504, 400, 403, 404, 408]
+
+# Edit values from here
+
+## TIME RELATED PARAMETERS
+# Delay between requests (in seconds)
+DOWNLOAD_DELAY = 0.5
+# Number of seconds to wait until considering a request timed out
+DOWNLOAD_TIMEOUT = 2
+
+#### The following parameters can't be commented out
+
+## STORAGE PARAMETERS
+MONGODB_SERVER = 'localhost'
+MONGODB_PORT = 27017
+MONGODB_DB = 'linkedin'
+MONGODB_COLLECTION = 'companies'
+MONGODB_UNIQ_KEY = '_id'
+
+
+## PROXY PARAMETERS
+# Absolute path where a proxy list can be found
+# If you want to disable proxy usage, set this variable to None
+PROXY_FILE_PATH = None
+# Minimum depth required to use a proxy
+MIN_LEVEL_FOR_PROXY = 0
+# Chance (up to 10) to use a proxy
+PROXY_CHANCE = 3
+# Minimum number of failed attempts required to discard a proxy
+# In this example, once a proxy has failed to answer a request, it will never
+# be used again.
+MIN_ATTEMPTS = 1
+
+## CONTENT PARAMETERS
+# A string all the letters that will be looked up in the directory
+# Each letter implies that every companie with that character at the beginning
+# of its name will be retrieved.
+
+# For example: COMPANY_LETTERS='az'
+# All the companies whose name start with 'a' will be retrieved and so will be
+# those who start with z.
+#COMPANY_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+COMPANY_LETTERS = 'z'
